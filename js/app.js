86c6871fa484e312a2c57b76b72e7906eb701386
Javier Castillo <javiercastillo.tuc@gmail.com>
2025-05-25 22:20:59 -0300

    falta solo el boton eliminar que elimine del localstorage y no solo del dom

---
+++ js/app.js
@@ -0,0 +1,78 @@
+let formulario = document.getElementById("formulario")
+let contenedorBoton = document.getElementById("contenedorBoton")
+let btnAgregar = document.getElementById("btnAgregar")
+let ul = document.getElementById("ul")
+let btnBorrarTodo = document.createElement("button")
+
+cargarDOM()
+
+btnAgregar.addEventListener("click", (e)=>{
+    e.preventDefault()
+    let input = document.getElementById("input").value
+    const li = document.createElement("li")
+    li.classList.add("list-group-item", "d-flex", "align-items-center", "justify-content-between")
+    li.textContent = input
+    ul.appendChild(li)
+    guardar(input)
+
+    const btnEliminar = document.createElement("button")
+    btnEliminar.classList.add("btn", "btn-outline-danger")
+    btnEliminar.textContent = "Eliminar"
+
+    li.appendChild(btnEliminar)
+
+    formulario.reset()
+
+    btnEliminar.addEventListener("click", ()=>{
+        li.remove()
+    })
+
+    
+    btnBorrarTodo.classList.add("btn", "btn-danger")
+    btnBorrarTodo.textContent = "Borrar Lista"
+    btnBorrarTodo.addEventListener("click", ()=>{
+        localStorage.removeItem('miLista')
+        
+    })
+    contenedorBoton.appendChild(btnBorrarTodo)
+    
+    
+})
+
+function guardar(input){
+    let lista = JSON.parse(localStorage.getItem('miLista')) || []
+    lista.push(input)
+    localStorage.setItem('miLista', JSON.stringify(lista) )
+}
+
+function cargarDOM(){
+    let lista = JSON.parse(localStorage.getItem('miLista')) || []
+    lista.forEach(itemLista => {
+        const li = document.createElement("li")
+        li.classList.add("list-group-item", "d-flex", "align-items-center", "justify-content-between")
+        li.textContent = itemLista
+        ul.appendChild(li)
+
+        const btnEliminar = document.createElement("button")
+        btnEliminar.classList.add("btn", "btn-outline-danger")
+        btnEliminar.textContent = "Eliminar"
+
+        li.appendChild(btnEliminar)
+
+
+        btnEliminar.addEventListener("click", ()=>{
+            li.remove()
+        })
+
+        
+        btnBorrarTodo.classList.add("btn", "btn-danger")
+        btnBorrarTodo.textContent = "Borrar Lista"
+        btnBorrarTodo.addEventListener("click", ()=>{
+            localStorage.removeItem('miLista')
+        })
+        contenedorBoton.appendChild(btnBorrarTodo)
+        
+    });
+
+}
+
